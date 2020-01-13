984a82ca9ea174bce8c186697609cb34de83fc99
Xu Yiming <xuyiming.open@outlook.com>
2020-01-13 14:30:02 +0800

    refactor: Not use babel-traverse for finding variables

--- main.js
+++ main.js
@@ -75,60 +75,35 @@ function* resolve (babel, path, state) {
 /** @type {BabelPlugin<any>} */
 const plugin = function (babel) {
   const t = babel.types
-  const traverse = babel.traverse
 
   /**
-   * @template {Node} T
-   * @callback CollectGVar
-   * @param {T} node
-   * @param {Scope} scope
-   * @returns {void}
+   * @param {import('@babel/types').LVal} lval
+   * @returns {IterableIterator<import('@babel/types').Identifier>}
    */
-
-  /** @type {CollectGVar<Node>} */
-  function testGVar (node, scope) {
-    if (t.isIdentifier(node) && this.isGVar(node.name, scope)) {
-      this.addGVar(node.name)
-    }
-  }
-
-  const gvarExtractor = {
-    /** @type {CollectGVar<import('@babel/types').ArrayPattern>} */
-    ArrayPattern (node, scope) {
-      node.elements.forEach(e => testGVar.call(this, e, scope))
-    },
-
-    /** @type {CollectGVar<import('@babel/types').RestElement>} */
-    RestElement (node, scope) {
-      testGVar.call(this, node.argument, scope)
-    },
-
-    /** @type {CollectGVar<import('@babel/types').ObjectProperty>} */
-    ObjectProperty (node, scope) {
-      testGVar.call(this, node.value, scope)
-    },
-
-    /** @type {CollectGVar<import('@babel/types').AssignmentPattern>} */
-    AssignmentPattern (node, scope) {
-      testGVar.call(this, node.left, scope)
-    }
-  }
-
-  const patternVisitor =
-    Object.fromEntries(Object.entries(gvarExtractor).map(
-      /** @returns {[string, import('@babel/traverse').VisitNodeFunction<any, Node>]} */
-      ([type, collect]) =>
-        [type, function (path) { collect.call(this, path.node, path.scope) }]
-    ))
-
-  /** @type {CollectGVar<Node>} */
-  function visit (node, scope) {
-    return Object.keys(gvarExtractor).forEach(
-      type => {
-        if (t.is(type, node)) {
-          gvarExtractor[type].call(this, node, scope)
+  function* variables (lval) {
+    if (t.isIdentifier(lval)) {
+      yield lval
+    } else if (t.isArrayPattern(lval)) {
+      for (const element of lval.elements) {
+        if (element) {
+          yield* variables(element)
         }
-      })
+      }
+    } else if (t.isRestElement(lval)) {
+      yield* variables(lval.argument)
+    } else if (t.isObjectPattern(lval)) {
+      for (const property of lval.properties) {
+        if (t.isObjectProperty(property)) {
+          yield* variables(
+            /** @type {import('@babel/types').PatternLike} */(property.value)
+          )
+        } else if (t.isRestElement(lval)) {
+          yield* variables(lval.argument)
+        }
+      }
+    } else if (t.isAssignmentPattern(lval)) {
+      yield* variables(lval.left)
+    }
   }
 
   return {
@@ -149,15 +124,11 @@ const plugin = function (babel) {
       this.addGVar = gvar => lvals.add(gvar)
     },
     visitor: {
-      AssignmentExpression (path, state) {
-        const lval = path.node.left
-
-        if (t.isIdentifier(lval) && this.isGVar(lval.name, path.scope)) {
-          this.addGVar(lval.name)
-        } else if (t.isPattern(lval)) {
-          // traverse() won't visit parent node
-          visit.call(this, lval, path.scope, state)
-          traverse(lval, patternVisitor, path.scope, state, path.parentPath)
+      AssignmentExpression (path) {
+        for (const gvar of variables(path.node.left)) {
+          if (this.isGVar(gvar.name, path.scope)) {
+            this.addGVar(gvar.name)
+          }
         }
       },
       Program: {
